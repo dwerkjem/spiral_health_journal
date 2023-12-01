33cad896f45537c03f3ceae95b637dc63efed593
dwerkjem <derekrneilson@gmail.com>
2023-12-01 16:55:45 -0700

    Subject: Capitalized venv in gitignore; setup argparser; added journaling module
    Body:
    
    This commit makes three primary adjustments:
    
    1. Capitalized '.venv-SpiralHTJ' in .gitignore. This standardizes our virtual environment naming convention across different machines where casing might vary.
    
    2. Implemented argparse in a new file, 'argparser.py'. This module prepares the groundwork for enjoying command-line functionality and user-friendly interaction.
    
    3. Introduced an empty 'journaling.py' module. This space anticipates the impending health journal tracking features and will house the related logic in future iterations.
    
    These underpinning changes set the stage for enhancing user experience and paving the way for a more robust structure.

---
+++ src/argparser.py
@@ -0,0 +1,6 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="entry point for spiral HTJ 'health tracking journal'")
+
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.0')
