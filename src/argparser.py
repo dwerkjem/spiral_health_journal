6eb1776b67b1a73f6ac5ce8793a8de8e3459bce7
dwerkjem <derekrneilson@gmail.com>
2023-12-01 18:57:23 -0700

    Subject: Enhanced logging and argument parsing capabilities
    Body:
    Modified argparser.py to significantly boost its logging and argument parsing abilities.
    Essential updates include:
    - Addition of colored logging associated with different levels (error, warning, info, debug), which improves readability and enables quicker identification of issues.
    - Introduction of a verbosity level control, providing developers with granular control over what type of messages they want to see during debugging and in what quantity.
    - Debug mode can now be enabled straight from the argument parser, allowing for enhanced debugging flexibility.
    - README and LICENSE references in the header comments for easier navigation.
    
    The .gitignore file was updated, removing an unnecessary line.
    
    These enhancements result in a more developer-friendly environment where issues can be spotted and fixed more efficiently, while the flexibility of the application is maintained and improved.

--- src/argparser.py
+++ src/argparser.py
@@ -1,6 +1,73 @@
+# Purpose: entry point for spiral HTJ 'health tracking journal'
+# see README.md for more details
+# Author: @dwerkjem (Derek R Neilson)'
+# License: OPENish v1.0 (see LICENSE.md for more details)
+# Version: 0.1.0
+# DO NOT EDIT ANYTHING ABOVE THIS LINE
 import argparse
 import sys
+import logging
 
 parser = argparse.ArgumentParser(description="entry point for spiral HTJ 'health tracking journal'")
 
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.0')
+parser.add_argument('-v', '--version', action='version', version=' 0.1.0')
+parser.add_argument('-d', '--debug', action='store_true', help='enable debug mode')
+parser.add_argument('-V', '--verbosity', type=int, default=1, help='increase output verbosity (max 3, min 0)')
+
+red = '\033[91m' # error
+yellow = '\033[93m' # warning
+green = '\033[92m' # info
+blue = '\033[94m' # debug
+white = '\033[0m' # rest of text
+colors = [red, yellow, green, blue, white]
+# add logging default to error and format to timestamp:  levelname: message ! for errors and warnings only
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+log = logging.getLogger(__name__) # get logger for this file
+# add color to logging levels
+logging.addLevelName(logging.ERROR, red + logging.getLevelName(logging.ERROR) + white)
+logging.addLevelName(logging.WARNING, yellow + logging.getLevelName(logging.WARNING) + white)
+logging.addLevelName(logging.INFO, green + logging.getLevelName(logging.INFO) + white)
+logging.addLevelName(logging.DEBUG, blue + logging.getLevelName(logging.DEBUG) + white)
+
+def set_verbosity(verbosity):
+    log.debug('setting verbosity level to %s', verbosity)
+    if verbosity == 0:
+        log.setLevel(logging.ERROR)
+        log.info('verbosity level set to 0, only errors will be shown')
+    elif verbosity == 1:
+        log.setLevel(logging.WARNING)
+        log.info('verbosity level set to 1, errors and warnings will be shown')
+    elif verbosity == 2:
+        log.setLevel(logging.INFO)
+        log.info('verbosity level set to 2, errors, warnings, and info will be shown')
+    elif verbosity == 3:
+        log.setLevel(logging.DEBUG)
+        log.info('verbosity level set to 3, errors, warnings, info, and debug will be shown')
+    else:
+        log.error('invalid verbosity level: %s', verbosity)
+        return False
+    log.debug('verbosity level set to %s', verbosity)
+    return True
+
+def parse_args():
+    args = parser.parse_args()
+    set_verbosity(args.verbosity)
+    log.debug('args: %s', args)
+    if args.debug:
+        log.info('debug mode enabled extra debug actions will be performed')
+    return True
+
+def start():
+    log.debug('starting spiral HTJ')
+    if parse_args():
+        log.debug('args parsed successfully')
+    else:
+        log.error('failed to parse args')
+        sys.exit(1)
+    log.debug('spiral HTJ started successfully')
+    return True
+
+if __name__ == '__main__':
+    if start():
+        log.info('spiral HTJ exited successfully and gracefully')
+    sys.exit(0)
